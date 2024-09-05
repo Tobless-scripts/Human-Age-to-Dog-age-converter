85e61ef49e7497269014da65716f467d0f9574d9
Tobless-scripts <obayomitaofeek7@gmail.com>
2024-09-05 02:07:53 -0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function laterAge(){
+    let userName = document.getElementById("userName").value;
     let myAge = parseInt(document.getElementById("myAge").value);
     let earlyYears = 2*10.5;
     let laterYears = (myAge - 2)*4;
     const myAgeInDogYears = earlyYears + laterYears;
-    document.getElementById("dogAge").innerHTML = "Your Age in Dog Years: " + myAgeInDogYears + "Years";
+    document.getElementById("dogAge").innerHTML = userName + "'s age in Dog Years: " + myAgeInDogYears + "Years Old";
 }
